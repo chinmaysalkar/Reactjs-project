ec83d605bcef67a476b4bfefb5cec627c0a76d65
PratikRangari <rangaripratik1998@gmail.com>
2024-02-19 18:39:57 +0530

    changes

--- src/pages/HRMS/Department.js
+++ src/pages/HRMS/Department.js
@@ -5,6 +5,9 @@ import ListView from '../../components/Department/ListView';
 // import Gridlist from '../../components/Department/Gridlist';
 import { Button, Modal } from 'react-bootstrap';
 import Sidebar from '../../layouts/Sidebar';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
 
 export default function Department() {
   const [activeButton, setActiveButton] = useState('listview');
@@ -21,6 +24,42 @@ export default function Department() {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
     
+
+
+    const initialValues = {
+      departmentName: '',
+      departmentHead: '',
+      numberOfEmployees: ''
+    };
+  
+    const validationSchema = Yup.object().shape({
+      departmentName: Yup.string().required('Department Name is required'),
+      departmentHead: Yup.string().required('Department Head is required'),
+      numberOfEmployees: Yup.number().required('Number of Employees is required').positive('Number of Employees must be positive').integer('Number of Employees must be an integer')
+    });
+  
+    const handleSubmit = (values, { resetForm }) => {
+      const newErrors = {};
+      if (!values.departmentName) {
+        newErrors.departmentName = 'Department Name is required';
+      }
+      if (!values.departmentHead) {
+        newErrors.departmentHead = 'Department Head is required';
+      }
+      if (!values.numberOfEmployees) {
+        newErrors.numberOfEmployees = 'Number of Employees is required';
+      }
+    
+  
+      if (Object.keys(newErrors).length === 0) {
+        // If no errors, handle form submission
+        console.log(values);
+        resetForm();
+        handleClose();
+      }
+    };
+
+
   return (
 
     <>
@@ -48,31 +87,43 @@ export default function Department() {
     </div>
    
     <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Add Department</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <div class="mb-3">
-          <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Deparments Name "/>
-          </div>
+            <Modal.Header closeButton>
+              <Modal.Title>Add Department</Modal.Title>
+            </Modal.Header>
+            <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleSubmit}>
+              {({ dirty, isValid }) => (
+                <Form>
+                  <Modal.Body>
+                    <div className="mb-3">
+                      <label htmlFor="departmentName" className="form-label">Department Name</label>
+                      <Field type="text" className="form-control" id="departmentName" name="departmentName" placeholder="Department Name" />
+                      <ErrorMessage name="departmentName" component="div" className="text-danger" />
+                    </div>
 
-          <div class="mb-3">
-          <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Deparments Head "/>
-          </div>
+                    <div className="mb-3">
+                      <label htmlFor="departmentHead" className="form-label">Department Head</label>
+                      <Field type="text" className="form-control" id="departmentHead" name="departmentHead" placeholder="Department Head" />
+                      <ErrorMessage name="departmentHead" component="div" className="text-danger" />
+                    </div>
 
-          <div class="mb-3">
-          <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="No of Employee "/>
-          </div>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose} className='bg-danger border-0'>
-            Cancel
-          </Button>
-          <Button variant="primary btn-top" className='border-0'>
-            Add
-          </Button>
-        </Modal.Footer>
-    </Modal>
+                    <div className="mb-3">
+                      <label htmlFor="numberOfEmployees" className="form-label">Number of Employees</label>
+                      <Field type="number" className="form-control" id="numberOfEmployees" name="numberOfEmployees" placeholder="Number of Employees" />
+                      <ErrorMessage name="numberOfEmployees" component="div" className="text-danger" />
+                    </div>
+                  </Modal.Body>
+                  <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose} className='bg-danger border-0'>
+                      Close
+                    </Button>
+                    <Button variant="primary btn-top" type="submit" className='border-0'>
+                      Add
+                    </Button>
+                  </Modal.Footer>
+                </Form>
+              )}
+            </Formik>
+          </Modal>
 
 
     <Footer/>
