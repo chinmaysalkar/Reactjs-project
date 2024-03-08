99ec9b586a9b8865e8a289cc04c18f5e5f1b96c0
PratikRangari <rangaripratik1998@gmail.com>
2024-03-08 19:01:16 +0530

    changes

---
+++ src/common/data/Project/projectlist.js
@@ -0,0 +1,155 @@
+import avatar2 from '../../../assets/images/avatar2.jpg'
+import avatar1 from '../../../assets/images/avatar1.jpg'
+import avatar3 from '../../../assets/images/avatar3.jpg'
+
+
+const projectonData = [
+    {
+      title: "New Admin Design",
+      tag: "Web Design",
+      description: "Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis temporibus!",
+      created: "09 Jun 2019 11:32AM",
+      creator: "Nathan Guerrero",
+      question: 23,
+      comments: 32,
+      bug: 5,
+      team: [avatar1, avatar2, avatar1, avatar1, avatar3],
+      progress: 15,
+      progressColor: "bg-red"
+    },
+    {
+      title: "Job Portal Web App",
+      tag: "Angular",
+      description: "Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis temporibus!",
+      created: "09 Jun 2019 11:32AM",
+      creator: "Nathan Guerrero",
+      question: 55,
+      comments: 43,
+      bug: 5,
+      team: [avatar1, avatar2, avatar1, avatar1, avatar3],
+      progress: 75,
+      progressColor: "bg-green"
+    },
+
+    {
+        title: "App Design and Development",           
+        tag: "Android",
+        description: "Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis temporibus!",
+        created: "09 Jun 2019 11:32AM",
+        creator: "Nathan Guerrero",
+        question: 55,
+        comments: 43,
+        bug: 5,
+        team: [avatar1, avatar2, avatar1, avatar1, avatar3],
+        progress: 75,
+        progressColor: "bg-green"
+      },
+
+      {
+        title: "One  Page Landing",
+        tag: "Wordpress",
+        description: "Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis temporibus!",
+        created: "09 Jun 2019 11:32AM",
+        creator: "Nathan Guerrero",
+        question: 55,
+        comments: 43,
+        bug: 5,
+        team: [avatar1, avatar2, avatar1, avatar1, avatar3],
+        progress: 75,
+        progressColor: "bg-green"
+      },
+
+      {
+        title: "Job Portal Web App",
+        tag: "iOS App",
+        description: "Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis temporibus!",
+        created: "09 Jun 2019 11:32AM",
+        creator: "Nathan Guerrero",
+        question: 55,
+        comments: 43,
+        bug: 5,
+        team: [avatar1, avatar2, avatar1, avatar1, avatar3],
+        progress: 85,
+        progressColor: "bg-green"
+      },
+  ];
+
+
+
+
+  const projectupData = [
+    {
+      owner: 'Isidore Dilao',
+      milestone: 'Account receivable',
+      avatar: avatar1,
+      work: '30:00',
+      duration: '30:0 hrs',
+      priority: 'Medium',
+      action: ''
+    },
+    {
+      owner: 'Maricel Villalon',
+      milestone: 'Account receivable',
+      avatar: avatar2,
+      work: '68:00',
+      duration: '105:0 hrs',
+      priority: 'High',
+      action: ''
+    },
+
+    {
+        owner: 'Maricel Villalon',
+        milestone: 'Account receivable',
+        avatar: avatar1,
+        work: '68:00',
+        duration: '105:0 hrs',
+        priority: 'None',
+        action: ''
+    },
+
+
+    {
+        owner: 'Maricel Villalon',
+        milestone: 'Account receivable',
+        avatar: avatar3,
+        work: '68:00',
+        duration: '105:0 hrs',
+        priority: 'None',
+        action: ''
+    },
+    
+    {
+        owner: 'Maricel Villalon',
+        milestone: 'Account receivable',
+        avatar: avatar1,
+        work: '68:00',
+        duration: '105:0 hrs',
+        priority: 'High',
+        action: ''
+    }, 
+
+
+    {
+        owner: 'Maricel Villalon',
+        milestone: 'Account receivable',
+        avatar: avatar1,
+        work: '68:00',
+        duration: '105:0 hrs',
+        priority: 'High',
+        action: ''
+    }, 
+
+
+    {
+        owner: 'Maricel Villalon',
+        milestone: 'Account receivable',
+        avatar: avatar3,
+        work: '68:00',
+        duration: '105:0 hrs',
+        priority: 'High',
+        action: ''
+    }, 
+    
+  ];
+
+export default (projectonData,projectupData);
